c0a302217311667052ed63c5729b6ceea4d2ffb6
Башлыков Данила <dbashlykov@tec>
2024-05-27 10:26:02 +0300

    commit_new

--- Operations.java
+++ Operations.java
@@ -1,5 +1,13 @@
 public class Operations {
 
+    private static int option(int n){
+        int k = 1;
+        for (int i = 1; i <= n; i++){
+            k *= i;
+        }
+        return k;
+    }
+
     public static void main(String[] args) {
 
         int a = 10, b = 5;
@@ -8,5 +16,8 @@ public class Operations {
         System.out.println(a*b);
         System.out.println(a/b);
         System.out.println(a-b);
+
+        System.out.println(option(6));
     }
+
 }
